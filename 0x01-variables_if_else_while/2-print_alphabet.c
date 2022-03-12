5fe2340138d15b694de8720c34ed3fc414d2904a
Abdulkarim Getachew <93995267+Mr-MaNia7@users.noreply.github.com>
2022-03-12 13:36:31 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
+/**
+* main - entry point
+* Return: program finished
+*/
 int main()
 {
   for (int i=97;i<97+26;i++)
     putchar(i);
-    putchar('\n');
- 
-    
-    
+  putchar('\n');
   return 0;
 }
