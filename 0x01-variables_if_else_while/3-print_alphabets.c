8dadac797f77dca01e299e84cef727a07810e4d1
Abdulkarim Getachew <93995267+Mr-MaNia7@users.noreply.github.com>
2022-03-12 14:40:48 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-
-int main()
+/**
+* main - entry point
+* Return: program finished
+*/
+int main(void)
 {
-  for (int i=97;i<97+26;i++)
-    putchar(i);
-        
-  for (int i=65;i<65+26;i++)
-    putchar(i);
-    putchar('\n');
-    
-    
-    
-  return 0;
+	for (int i=97;i<97+26;i++)
+		putchar(i);
+	for (int i=65;i<65+26;i++)
+		putchar(i);
+	putchar('\n');
+	return 0;
 }
