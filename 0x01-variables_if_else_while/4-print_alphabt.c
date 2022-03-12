1955b732e88c15048145983ac0f20e9a0953a165
Abdulkarim Getachew <93995267+Mr-MaNia7@users.noreply.github.com>
2022-03-12 14:53:21 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-
-int main()
+/** 
+* main - entry point
+* Return: program finished
+*/
+int main(void)
 {
-  for (int i=97;i<97+26;i++){
-        
-    if (i == 101 || i ==17+96 )
-      continue;
-    putchar(i);
-        
-  }
-        
-  putchar('\n');
-    
-  return 0;
+	int i;
+
+	for (i=97; i < 97 + 26; i++)
+	{
+		if (i == (101 || 97 + 17))
+			continue;
+		putchar(i);
+	}
+	return (0);
 }
