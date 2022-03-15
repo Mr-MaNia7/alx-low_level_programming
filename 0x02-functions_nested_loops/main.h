30d581ca0dc01bdc9e6c30a191fde76e18e1ed99
Abdulkarim Getachew <93995267+Mr-MaNia7@users.noreply.github.com>
2022-03-15 16:50:25 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,19 @@
 int _putchar(char c);
 void print_alphabet(void);
+/**
+ * print_alphabet - prints lower case alphabets
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	i=97;
+	while(i < 97 + 26)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
