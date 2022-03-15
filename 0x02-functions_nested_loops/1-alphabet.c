9ef86bfb2303909437730409a74563d16fe1aab8
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2022-03-15 06:24:56 -0700

    Header files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-
-void print_alphabet(void);
+#include "main.h"
 
 /**
  * main - Entry point
@@ -11,22 +10,4 @@ int main(void)
 {
 	print_alphabet();
 	return (0);
-}
-
-/**
- * print_alphabet - Prints lower case alphabets
- *
- * Return: void
- */
-void print_alphabet(void)
-{
-	int i;
-
-	i=97;
-	while(i < 97 + 26)
-	{
-		putchar(i);
-		i++;
-	}
-	putchar('\n');
 }
