7da7dec7778880981df7c2cb0595813fca15dc45
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2022-03-16 19:15:39 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+/**
+ * _abs - calcualtes absolute value
+ * @n: number to be calculated
+ * Return: absolute value of numbers
+*/
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
