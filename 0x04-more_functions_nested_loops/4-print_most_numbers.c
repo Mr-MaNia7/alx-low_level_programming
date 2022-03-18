9253822c8c5c3d155b21d617e4c509cf79fb79a4
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2022-03-17 18:58:39 -0700

    Most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers
+ *
+ * Return: numbers
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (c == 52 || c == 50)
+			continue;
+		_putchar(i);
+	}
+	_putchar('\n');
+}
