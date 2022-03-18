4d08284b8939bac80d3245b8609476cd23084800
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2022-03-18 06:03:09 -0700

    Diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+include "main.h"
+
+/**
+ * print_diagonal - Prints diagonal wrt the number entered
+ * @n: Integer to be entered
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(92);
+	}
+	_putchar('\n');
+}
