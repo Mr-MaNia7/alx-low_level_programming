39d5f504b58cc3d4d1fd88fbccbffd8a2ef0588e
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2022-03-17 19:15:58 -0700

    More Numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+ *
+ * Return: numbers up to 14 10 times
+ */
+void more_numbers(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+	for (j = 48; j < 58; j++)
+		_putchar(j);
+	for (k = 53; k < 57; k++)
+		_putchar(k + 5);
+	_putchar('\n');
+	}
+}
