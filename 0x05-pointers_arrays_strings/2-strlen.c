47630812440d524cb981a28139aa118c3631d814
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2022-03-22 06:15:40 -0700

    Strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,11 +10,11 @@ int _strlen(char *s)
 	int len;
 
 	len = 0;
-	while(len >= 0)
+	while (len >= 0)
 	{
-		if(s[len] == '\0')
+		if (s[len] == '\0')
 			break;
 		len++;
 	}
-	return len;
+	return (len);
 }
