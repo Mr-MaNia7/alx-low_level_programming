7707cdaebfe738041f451ca8d87b1b65581edb9d
Abdulkarim Getachew <93995267+Mr-MaNia7@users.noreply.github.com>
2022-03-23 04:01:49 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,7 +7,7 @@
  */
 void puts_half(char *str)
 {
-	int len, idx, l;
+	int len, idx;
 
 	len = 0;
 	while (len >= 0)
@@ -17,16 +17,15 @@ void puts_half(char *str)
 		len++;
 	}
 
-	l = len;
-	if (l % 2 == 0)
+	if (len % 2 == 0)
 	{
-		for (idx = l / 2; idx <= l; idx++)
+		for (idx = len / 2; idx < len; idx++)
 			_putchar(str[idx]);
 	}
 
 	else
 	{
-		for (idx = (l + 1) / 2; idx <= l; idx++)
+		for (idx = (len + 1) / 2; idx < len; idx++)
 			_putchar(str[idx]);
 	}
 	_putchar('\n');
