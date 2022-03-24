d8df78e96c3d4cc065c061d9504daf3efbc0a728
Abdulkarim Getachew <93995267+Mr-MaNia7@users.noreply.github.com>
2022-03-24 08:09:01 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,7 +7,7 @@
 * @n: The number of characters to append
 * Return: Pointer to dest
 */
-char *_strcat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int desti, srci;
 
